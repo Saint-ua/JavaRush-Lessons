b269c7562be74d7090d964b6e1f5bbfd47657582
Saint-ua <koss4ever@gmail.com>
2022-09-30 14:22:36 +0100

    Refactoring

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task17/task1716/Car.java
@@ -0,0 +1,7 @@
+package ua.javarush.task.pro.task17.task1716;
+
+public class Car implements Vehicle {
+    public void move() {
+        System.out.println("Їду із середньою швидкістю 70 км/год.");
+    }
+}
