5564668c5c3ddd2f93fa8a79058737352a50502a
Saint-ua <koss4ever@gmail.com>
2022-10-03 20:01:50 +0100

    Refactoring

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task18/task1816/Solution.java
@@ -0,0 +1,20 @@
+package ua.javarush.task.pro.task18.task1816;
+
+import java.util.stream.Stream;
+
+/* 
+Перетворення даних-2
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Stream<String> stream = Stream.of("Не", "хвилюйтеся", "якщо", "щось", "не", "працює.", "Коли", "б", "усе", "працювало", "вас", "би", "звільнили.");
+
+        toUpperCase(stream).forEach(System.out::println);
+    }
+
+    public static Stream<String> toUpperCase(Stream<String> strings) {
+        return strings.map(x -> x.toUpperCase());
+    }
+}
