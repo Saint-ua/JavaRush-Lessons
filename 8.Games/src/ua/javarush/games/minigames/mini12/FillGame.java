d40e1200a119c233d08803053d303eb1dbecf1aa
Saint-ua <koss4ever@gmail.com>
2022-10-07 23:49:07 +0100

    Refactoring

---
+++ 8.Games/src/ua/javarush/games/minigames/mini12/FillGame.java
@@ -0,0 +1,39 @@
+package ua.javarush.games.minigames.mini12;
+
+import com.javarush.engine.cell.Color;
+import com.javarush.engine.cell.Game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Працюємо зі списком координат
+*/
+
+public class FillGame extends Game {
+    private final List<Cell> cells = new ArrayList<>();
+
+    @Override
+    public void initialize() {
+        setScreenSize(10, 10);
+        for (int x = 0; x < 10; x++) {
+            for (int y = 0; y < 10; y++) {
+                setCellColor(x, y, Color.WHITE);
+            }
+        }
+    }
+
+    @Override
+    public void onMouseLeftClick(int x, int y) {
+        setCellColor(x,y,Color.ORANGE);
+        Cell newCell = new Cell(x,y);
+        if (!cells.contains(newCell)) cells.add(newCell);
+    }
+
+    @Override
+    public void onMouseRightClick(int x, int y) {
+        setCellColor(x,y,Color.WHITE);
+        Cell newCell = new Cell(x,y);
+        if (cells.contains(newCell)) cells.remove(newCell);
+    }
+}
