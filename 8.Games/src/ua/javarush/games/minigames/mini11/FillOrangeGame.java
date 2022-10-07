52802106891b8e9b0e7d713d3913a613392410e1
Saint-ua <koss4ever@gmail.com>
2022-10-07 23:21:55 +0100

    Refactoring

---
+++ 8.Games/src/ua/javarush/games/minigames/mini11/FillOrangeGame.java
@@ -0,0 +1,18 @@
+package ua.javarush.games.minigames.mini11;
+
+import com.javarush.engine.cell.Color;
+
+/* 
+Зафарбовуємо поле
+*/
+
+public class FillOrangeGame extends FillGame {
+    @Override
+    public void fill() {
+        for (int x = 0; x < 5; x++) {
+            for (int y = 0; y < 5; y++) {
+                setCellColor(x, y, Color.ORANGE);
+            }
+        }
+    }
+}
