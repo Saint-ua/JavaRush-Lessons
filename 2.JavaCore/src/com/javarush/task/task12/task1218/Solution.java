28b7c74b5fdbfa95f07a6e4e109f2efa38a94e4f
Saint-ua <koss4ever@gmail.com>
2022-10-09 13:45:30 +0100

    Refactoring

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1218/Solution.java
@@ -0,0 +1,71 @@
+package com.javarush.task.task12.task1218;
+
+/* 
+Есть, летать и двигаться
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        public void fly();
+    }
+
+    public interface CanMove {
+        public void move();
+    }
+
+    public interface CanEat {
+        public void eat();
+    }
+
+    public class Dog implements CanMove, CanEat {
+        @Override
+        public void move() {
+            return;
+        }
+
+        @Override
+        public void eat() {
+            return;
+        }
+    }
+
+    public class Duck implements CanMove, CanEat, CanFly {
+        @Override
+        public void fly() {
+            return;
+        }
+
+        @Override
+        public void move() {
+            return;
+        }
+
+        @Override
+        public void eat() {
+            return;
+        }
+    }
+
+    public class Car implements CanMove {
+        @Override
+        public void move() {
+            return;
+        }
+    }
+
+    public class Airplane implements CanMove, CanFly {
+        @Override
+        public void fly() {
+            return;
+        }
+
+        @Override
+        public void move() {
+            return;
+        }
+    }
+}
