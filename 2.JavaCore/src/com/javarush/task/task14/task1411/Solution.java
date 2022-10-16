6d98a2763207353b970cbaf5bc1728b9a98f64fb
Saint-ua <koss4ever@gmail.com>
2022-10-16 23:10:15 +0100

    Refactoring

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1411/Solution.java
@@ -0,0 +1,59 @@
+package com.javarush.task.task14.task1411;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import static com.javarush.task.task14.task1411.Person.*;
+
+/* 
+User, Loser, Coder and Proger
+*/
+
+public class Solution implements Person {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Person person = null;
+        String key = null;
+        while (true) {
+            key = reader.readLine();
+            boolean ifTrue = key.equals("user") || key.equals("loser") || key.equals("coder") || key.equals("proger");
+            if (!ifTrue) break;
+            
+            switch (key) {
+                case "user": {
+                    person = new Person.User();
+                    break;
+                }
+                case "loser": {
+                    person = new Person.Loser();
+                    break;
+                }
+                case "coder": {
+                    person = new Person.Coder();
+                    break;
+                }
+                case "proger": {
+                    person = new Person.Proger();
+                    break;
+                }
+            }
+            doWork(person);
+        }
+    }
+
+    public static void doWork(Person person) {
+        if (person instanceof Person.User) {
+            Person.User user = (Person.User) person;
+            user.live();
+        } else if (person instanceof Person.Loser) {
+            Person.Loser loser = (Person.Loser) person;
+            loser.doNothing();
+        } else if (person instanceof Person.Coder) {
+            Person.Coder coder = (Person.Coder) person;
+            coder.writeCode();
+        } else if (person instanceof Person.Proger) {
+            Person.Proger proger = (Person.Proger) person;
+            proger.enjoy();
+        }
+    }
+}
