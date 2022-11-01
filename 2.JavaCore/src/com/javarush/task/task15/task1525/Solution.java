b7df59f3114a1c17802ba48499c69489e853b50f
Saint-ua <koss4ever@gmail.com>
2022-11-01 21:41:09 +0000

    Refactoring

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task15.task1525;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Файл в статическом блоке
+*/
+
+public class Solution {
+    public static List<String> lines = new ArrayList<String>();
+    static{
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(Statics.FILE_NAME));
+            while (reader.ready()) {
+                String line = reader.readLine();
+                lines.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public static void main(String[] args) {
+        System.out.println(lines);
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Statics.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task15.task1525;
+
+import java.io.*;
+
+public class Statics {
+    public static String FILE_NAME = "D:\\KOSTA\\3. IT\\1. LEARNING\\JAVA\\Test-Folder\\readme.txt"/* add the path to your source file here */;
+}
