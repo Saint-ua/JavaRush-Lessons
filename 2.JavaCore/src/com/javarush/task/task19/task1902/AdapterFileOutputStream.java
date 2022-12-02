c447f643e937564c09657dd8ac787802a31cb52c
Saint-ua <koss4ever@gmail.com>
2022-12-02 18:43:57 +0000

    Refactoring

--- 2.JavaCore/src/com/javarush/task/task19/task1902/AdapterFileOutputStream.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1902/AdapterFileOutputStream.java
@@ -1,20 +1,40 @@
-//package com.javarush.task.task19.task1902;
-//
-//import java.io.FileOutputStream;
-//import java.io.IOException;
-//
-///*
-//Адаптер
-//*/
-//
-//public class AdapterFileOutputStream implements AmigoStringWriter {
-//    private File
-//
-//    public static void main(String[] args) {
-//
-//    }
-//
-//
-//
-//}
-//
+package com.javarush.task.task19.task1902;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/*
+Адаптер
+*/
+
+public class AdapterFileOutputStream implements AmigoStringWriter {
+    private FileOutputStream fileOutputStream;
+
+    @Override
+    public void flush() throws IOException {
+        this.fileOutputStream.flush();
+    }
+
+    @Override
+    public void writeString(String s) throws IOException {
+        this.fileOutputStream.write(s.getBytes());
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.fileOutputStream.close();
+    }
+
+    public AdapterFileOutputStream(FileOutputStream fileOutputStream) {
+        this.fileOutputStream = fileOutputStream;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
+
